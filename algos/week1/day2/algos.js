bf09817d56744ec91befae485248658630718d1c
Shawn Allen Converse <68838748+shawnaconverse@users.noreply.github.com>
2022-12-13 16:08:11 -0800

    w1d2

---
+++ algos/week1/day2/algos.js
@@ -0,0 +1,57 @@
+// VARIABLES
+
+// What are they?
+// something that place to store a value / a box
+
+// we declare new variables
+// we reassign existing variables
+// the = is called the assignment operator
+
+// variables can vary. they can be reused and we can swap out what is inside of them
+// using the var keyword is like buying a new box
+// variables have a label so we can know what is inside of them
+// the computer DOES NOT CARE what the name of the variable is (as long as there are no spaces)
+
+// we can only put data inside of a variable, we cannot put expressions
+
+var result = "2 + 2"
+console.log(result)
+
+var isThisAValidVariable = "yes or no?"
+
+console.log(isThisAValidVariable)
+
+// var kitchen = "Shawn's Kitchen"
+
+var how_about_this_is_THIS_A_VALID_VARIABLE = "maybe?"
+
+console.log(how_about_this_is_THIS_A_VALID_VARIABLE)
+
+var hjasdlfasdlkjfhacljvpnmapdsnmaslduohbahhsfdghasodugnaxpcvubhareuophgxponzmxcgnamsdokcnmbzxvcbnapzsznbxzlbnzfuogbnasfnbmvjzvcnbasfjnbapxfvcnbzuopfnaprsonpuognasdpghacxgasuodfnapsdgnacxpgnbasprouhga = "hahahaahahaahha"
+
+console.log(hjasdlfasdlkjfhacljvpnmapdsnmaslduohbahhsfdghasodugnaxpcvubhareuophgxponzmxcgnamsdokcnmbzxvcbnapzsznbxzlbnzfuogbnasfnbmvjzvcnbasfjnbapxfvcnbzuopfnaprsonpuognasdpghacxgasuodfnapsdgnacxpgnbasprouhga)
+
+var firstName = "Shawn"
+var lastName = "Converse"
+lastName = "Venegas"
+var lastname = "Yee"
+var name = "Shawn"
+'my quote is: "to be or not to be"'
+
+console.log(lastname)
+console.log("Brown")
+// Naming Convention
+// camelCase  the first word is lowercase and all other words are uppercase first letter
+
+// Data Types
+// Strings
+// text surrounded by either  a pair single quotes or a  pair double quotes
+
+// Numbers
+// whole numbers and decimal
+
+// Boolean
+// true or false
+
+// null -> nothing
+// undefined -> not defined
