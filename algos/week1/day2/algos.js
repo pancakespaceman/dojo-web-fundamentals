b437523cbf6f930b31eb376d635e3caae238e877
Shawn Allen Converse <68838748+shawnaconverse@users.noreply.github.com>
2023-04-12 09:09:51 -0700

    algo solutions

--- algos/week1/day2/algos.js
+++ algos/week1/day2/algos.js
@@ -1,11 +1,10 @@
+var smallerStringA1 = 'abcd';
+var smallerStringB1 = 'efg';
+var expected = 'efgabcd';
 
-var smallerStringA1 = 'abcd'
-var smallerStringB1 = 'efg'
-var expected = 'efgabcd'
-
-var smallerStringA2 = 'sunnyside up eggs'
-var smallerStringB2 = 'biscuits and gravy'
-var expected = 'sunnyside up eggsbiscuits and gravy'
+var smallerStringA2 = 'sunnyside up eggs';
+var smallerStringB2 = 'biscuits and gravy';
+var expected = 'sunnyside up eggsbiscuits and gravy';
 
 /**
  * Combine two given strings together with the smaller string being added
@@ -13,17 +12,22 @@ var expected = 'sunnyside up eggsbiscuits and gravy'
  * added to the front.
  * @returns {string} The combined strings.
  */
-function combineSmallerStringFirst(/* Replace this comment with the needed parameters. What info does this function need? */) {
-  // Replace this comment with the code needed to make this function work.
+function combineSmallerStringFirst(s1, s2) {
+  if (s1.length <= s2.length) {
+    return s1 + s2;
+  } else {
+    return s2 + s1;
+  }
 }
 
-var stringToRepeat1 = 'Birria Tacos'
-var numberToRepeat1 = 5
-var repeatedExpected = 'Birria TacosBirria TacosBirria TacosBirria TacosBirria Tacos'
+var stringToRepeat1 = 'Birria Tacos';
+var numberToRepeat1 = 5;
+var repeatedExpected =
+  'Birria TacosBirria TacosBirria TacosBirria TacosBirria Tacos';
 
-var stringToRepeat2 = 'margherita pizza'
-var numberToRepeat2 = 2
-var repeatedExpected = 'margherita pizzamargherita pizza'
+var stringToRepeat2 = 'margherita pizza';
+var numberToRepeat2 = 2;
+var repeatedExpected = 'margherita pizzamargherita pizza';
 
 /**
  * Given a string and an integer representing how many times the string should
@@ -31,33 +35,72 @@ var repeatedExpected = 'margherita pizzamargherita pizza'
  * many times.
  * @returns {string} The given string repeated the specified amount of times.
  */
-function stringRepeat() {}
+function stringRepeat(str, repeatAmount) {
+  let repeatedStr = '';
 
+  for (let i = 0; i < repeatAmount; i++) {
+    repeatedStr += str;
+  }
+  return repeatedStr;
+}
 
-
-var wordArray = ['shawn', 'jim', 'tyler', 'heidi', 'john', 'alfredo', 'michael']
+var wordArray = [
+  'shawn',
+  'jim',
+  'tyler',
+  'heidi',
+  'john',
+  'alfredo',
+  'michael',
+];
 
 /**
  * Determines the average length of the words in the given array.
  * @returns {number} The average length of the given words.
  */
-function avgWordLength() {}
+function avgWordLength(words) {
+  let totalLen = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    totalLen += words[i].length;
+  }
+
+  return totalLen / words.length;
+}
 
 /**
  * Finds the longest word in the given array of words.
  * @returns {string} The longest word. If there are multiple words with the same
  *    length, this should be the last word in the array with that length.
  */
-function findLongestWord() {}
+function findLongestWord(words) {
+  let longestWord = '';
 
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
 
+    if (word.length >= longestWord.length) {
+      longestWord = word;
+    }
+  }
 
-var rangeA1 = 10
-var rangeB1 = 15
-var rangeExpected = 75
+  return longestWord;
+}
+
+var rangeA1 = 10;
+var rangeB1 = 15;
+var rangeExpected = 75;
 /**
  * Calculates the sum of the given range, inclusive. I.e., the sum of the first
  * number through the last number, inclusive.
  * @returns {number} The sum of the given range, inclusive.
  */
-function inclusiveRangeSum() {}
+function inclusiveRangeSum(start, end) {
+  let sum = 0;
+
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
