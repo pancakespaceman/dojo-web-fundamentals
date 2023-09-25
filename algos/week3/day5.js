fcc3e0d5da61f9473e27ea8540f6a5b773c63565
Shawn Allen Converse <68838748+shawnaconverse@users.noreply.github.com>
2023-09-25 07:47:28 -0700

    last day

--- algos/week3/day5.js
+++ algos/week3/day5.js
@@ -1,24 +1,44 @@
 /**
   You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
-  You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+  You want to maximize your profit by choosing a single day to buy one stock and 
+  choosing a different day in the future to sell that stock.
 
   Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 */
 
+var prices1 = [7, 1, 5, 3, 6, 4]
+var expected1 = 5
+// Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+
+var prices2 = [5, 4, 3, 2, 1]
+var expected2 = 0
+// no profit can be made
+
+/**
+ * Take an array of prices and return the maximum profit
+ * @param {number[]} array of prices
+ * @return {number} max profit
+ */
+function maxProfit(prices) {}
+
+
 // =======================================================================================================
 
 /*
-  There is a collection of input strings and a collection of query strings. For each query string, determine how many times it occurs in the list of input strings. Return an array of the results.
-  Example
-  stringList = ['ab', 'ab', 'abc']
-  queries = ['ab', 'abc', 'bc']
-  There are  instances of '',  of '' and  of ''. For each query, add an element to the return array, .
-  Function Description
-  Complete the function matchingStrings in the editor below. The function must return an array of integers representing the frequency of occurrence of each query string in stringList.
-  matchingStrings has the following parameters:
-  string stringList[n] - an array of strings to search
-  string queries[q] - an array of query strings
-  Returns
-  int[q]: an array of results for each query
+  There is a collection of input strings and a collection of query strings. 
+  For each query string, determine how many times it occurs in the list of input strings. Return an array of the results.
+  
 */
+
+var stringList1 = ['ab', 'ab', 'abc']
+var queries1 = ['ab', 'abc', 'bc']
+var expected = [2, 1, 0]
+
+/**
+ * Return an array of integers representing the frequency of occurrence of each query string in string list 
+ * @param {string[]} array of strings
+ * @param {string[]} string to search for
+ * @return {number[]} number of instances of each query string
+ */
+function matchingStrings(stringList, queries) {}
