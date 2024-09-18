2c5de5fc90bd0082268b9adfbfeae320cf8ed7cd
Shawn Converse <shawn.a.converse@outlook.com>
2024-09-18 09:09:32 -0700

    w3d3

---
+++ algos/week3/day3.js
@@ -0,0 +1,37 @@
+/* 
+  Given array and an additional value, insert this value at the beginning of
+  the array by making space for it at the beginning of the array.
+  return the new length of the array.
+  
+  Do this without using any built-in array methods.
+*/
+
+var arr1 = [1, 2, 3];
+var newItem1 = 0;
+var expected1 = 4;
+// after function is called, arr1 should be:
+var arr1Expected = [0, 1, 2, 3];
+
+var arr2 = [];
+var newItem2 = "a";
+var expected2 = 1;
+// after function is called, arr2 should be:
+var arr2Expected = ["a"];
+
+/**
+ * Shifts all items to the right by one to make space to add the given new item
+ * to the front of the given array.
+ * - Time: O(n) - linear
+ * - Space: O(1) - constant (i didn't make a new array)
+ * @param {Array<any>} items An array of any kind of items.
+ * @param {any} newItem To add to front.
+ * @returns {number} The new length of items.
+ */
+function unshift(items, newItem) {
+  for (var i = items.length - 1; i >= 0; i--) {
+    items[i + 1] = items[i]
+  }
+  items[0] = newItem
+
+  return items.length
+}
